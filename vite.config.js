15b8a50206b53f96fa5a4ef36d10b73b3f5a856e
Timonsterer <69845907+Timonsterer@users.noreply.github.com>
2026-04-16 12:28:19 +0800

    total function upgrade 04161228

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     host: true,
     port: 5173,
-    allowedHosts: true, // 🔥 重點（新版寫法）
+    allowedHosts: true,
   },
 })
